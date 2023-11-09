5b2df13b6f765d1e6acc2c321c5eb586bf754651
Estudante Alura <105929920+Anaketlin@users.noreply.github.com>
2023-11-09 08:30:50 -0300

    .

--- main.js
+++ main.js
@@ -8,9 +8,8 @@ while(contador<listaDeTeclas.length){
     const efeito = listaDeTeclas[contador].classlist[1];
     const idAudio = "#som_"+efeito;
     listaDeTeclas[contador].onclick = function(){
-        tocaSom("#som_tecla_aplausos");
         tocaSom(idAudio);
     }
-    contador = contador+1;
+    contador = contador + 1;
     console.log(contador);
 }
